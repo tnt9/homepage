470879c39cc28df4a01921f7e7996ed2abde175e
Nina Tratnik <nina.tratnik@gmail.com>
2016-06-05 19:44:18 +0200

    base file

--- main.py
+++ main.py
@@ -14,27 +14,50 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import os
+import jinja2
 import webapp2
 
+template_dir = os.path.join(os.path.dirname(__file__), "templates")
+jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir), autoescape=False)
 
-class MainHandler(webapp2.RequestHandler):
+
+class BaseHandler(webapp2.RequestHandler):
+
+    def write(self, *a, **kw):
+        return self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        return self.write(self.render_str(template, **kw))
+
+    def render_template(self, view_filename, params=None):
+        if not params:
+            params = {}
+        template = jinja_env.get_template(view_filename)
+        return self.response.out.write(template.render(params))
+
+class MainHandler(BaseHandler):
     def get(self):
-        self.response.write('jujpiiiii! Hello world!')
+        return self.render_template("index.html")
 
 
-class BlogHandler(MainHandler):
+class BlogHandler(BaseHandler):
     def get(self):
-        self.response.write('jujpiiiii! Hello world!')
+        return self.render_template("blog.html")
 
 
-class ContactsHandler(MainHandler):
+class ContactsHandler(BaseHandler):
     def get(self):
-        self.response.write('jujpiiiii! Hello world!')
+        return self.render_template("contacts.html")
 
 
-class ProjectsHandler(MainHandler):
+class ProjectsHandler(BaseHandler):
     def get(self):
-        self.response.write('jujpiiiii! Hello world!')
+        return self.render_template("projects.html")
 
 
 app = webapp2.WSGIApplication([
